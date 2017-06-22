424aff32dc52eccadbf7956979b0b92a2af0e1d9
sp44s <philipshiju@rediffmail.com>
2017-06-22 15:57:22 +0530

    Added view and template for detailed blog as well as CSS for the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-        url( r'^$', views.post_list, name='post_list'),
+        url( r'^$', views.post_list, name='thiscanbeanything'),
+        url( r'^post/(?P<id>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -6,4 +6,7 @@ def post_list(request):
     post = Post.objects.all()
     return render( request, 'blog/post_list.html', {'NamedPosts':post})
 
-    
+
+def post_detail(request, id):
+    post = get_object_or_404( Post, pk=id)
+    return render( request, 'blog/post_detail.html', {'onepost':post})

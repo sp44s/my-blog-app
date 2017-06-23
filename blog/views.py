e0682c3abbab079549146cbe73a7fc76d144ff72
sp44s <philipshiju@rediffmail.com>
2017-06-23 09:49:53 +0530

    added views to create/edit blog post inside the site

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import Postform
 
 def post_list(request):
     post = Post.objects.all()
@@ -10,3 +11,36 @@ def post_list(request):
 def post_detail(request, id):
     post = get_object_or_404( Post, pk=id)
     return render( request, 'blog/post_detail.html', {'onepost':post})
+    
+    
+def post_new(request):
+    if (request.method == "POST"):
+        form = Postform(request.POST)
+        if (form.is_valid()):
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', id=post.pk)
+    else:
+        form = Postform()
+    return render( request, 'blog/post_edit.html', {'newform':form})
+
+def post_edit(request, id):
+    post = get_object_or_404(Post, pk=id)
+    if request.method == "POST":
+        form = Postform(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', id=post.pk)
+    else:
+        form = Postform(instance=post)
+    return render(request, 'blog/post_edit.html', {'newform': form})
+    
+
+
+    
+    
